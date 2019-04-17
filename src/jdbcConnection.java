95da27410bfe0e9c6a091bb977f78e0324ab3794
mohit <mohit@Alpha-Blue>
2019-04-18 01:01:03 +0530

    new jdbc function added

---
+++ src/jdbcConnection.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import javax.swing.*;
+public class jdbcConnection {
+    Connection conn=null;
+    public static Connection dbConnection() {
+            try {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Connection conn= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system","hr");
+                JOptionPane.showMessageDialog(null,"connected");
+                return conn;
+            } catch (ClassNotFoundException | SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+                return null;
+            }
+    }
+}
